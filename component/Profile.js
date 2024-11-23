c5f18ac307dd7fc441c74294a1345a5e37f6af16
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-23 12:48:40 +0530

    add ProfileComponent to display user profile information

---
+++ component/Profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Profile = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Profile Component</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
+
+export default Profile;
