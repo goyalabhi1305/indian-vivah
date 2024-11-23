8784f3fe53135a3042467d60c0f23233367d9d91
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-23 15:54:25 +0530

    add QucikSearch component for quick search functionality

---
+++ app/search/QucikSearch.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import QuickSearch from '../../component/QuickSearch'
+
+const QucikSearch = () => {
+  return (
+
+    <View>
+      <QuickSearch />
+    </View>
+  )
+}
+
+export default QucikSearch
+
+const styles = StyleSheet.create({})
