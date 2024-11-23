834c99027791a0c731064d0829ce1b7f07646f60
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-23 15:54:19 +0530

    add AdvanceSearch component for enhanced search functionality

---
+++ app/search/AdvanceSearch.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AdvancedSearch from '../../component/AdvancedSearch'
+
+const AdvanceSearch = () => {
+  return (
+    <View>
+      <AdvancedSearch />
+    </View>
+  )
+}
+
+export default AdvanceSearch
+
+const styles = StyleSheet.create({})
