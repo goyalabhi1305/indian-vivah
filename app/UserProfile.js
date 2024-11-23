f2b19dc5a068465d1875fe1ca3faf7dc1f4d009e
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-23 12:46:59 +0530

    Add UserProfile component to display user profile information

---
+++ app/UserProfile.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ProfilePage from '../component/Profile/ProfilePage'
+
+const UserProfile = () => {
+  return (
+    <View>
+      <ProfilePage />
+    </View>
+  )
+}
+
+export default UserProfile
+
+const styles = StyleSheet.create({})
