b9a103b5e00addcbc73cff605de47ff112f34fd4
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-27 16:10:28 +0530

    add background color to container and input fields for improved visibility

--- app/edit/BasicInfo.js
+++ app/edit/BasicInfo.js
@@ -38,9 +38,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+    backgroundColor: '#fff',
   },
   input: {
     marginBottom: 16,
+    backgroundColor: '#fff',
   },
 });
 
