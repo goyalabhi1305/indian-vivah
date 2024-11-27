7b5958483dde945a34f8b40a4f99c3803f5db2e7
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-27 16:12:59 +0530

    update SelfProfileComponent background color from light gray to white for improved visibility

--- component/SelfProfileComponent.js
+++ component/SelfProfileComponent.js
@@ -118,7 +118,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#fff',
     width: '100%',
   },
   profileImageContainer: {
