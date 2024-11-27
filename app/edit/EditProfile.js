c4de02ee3d526259d972e657b81ed34c0084c82b
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-27 16:10:35 +0530

    update background color of profile edit container for improved aesthetics

--- app/edit/EditProfile.js
+++ app/edit/EditProfile.js
@@ -91,7 +91,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#fff',
   },
   title: {
     marginBottom: 16,
