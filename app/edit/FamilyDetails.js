9e4ea94d407a26733ff32d93073057c034e42223
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-29 21:33:15 +0530

    enhance FamilyDetails component with data fetching, loading state, and improved form handling

--- app/edit/FamilyDetails.js
+++ app/edit/FamilyDetails.js
@@ -1,41 +1,142 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { TextInput, Button, ActivityIndicator } from 'react-native-paper';
+import { UserOnBoard } from '../../services/endpoint';
+import useSWR from 'swr';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const FamilyDetails = () => {
-  const [fatherName, setFatherName] = useState('');
-  const [motherName, setMotherName] = useState('');
-  const [siblings, setSiblings] = useState('');
+  const [fatherOccupation, setFatherOccupation] = useState('');
+
+  const [motherOccupation, setMotherOccupation] = useState('');
+  const [sitsters, setSisiters] = useState('');
+  const [brothers, setBrothers] = useState('');
   const [familyType, setFamilyType] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  const familyTypeArray = [
+    { label: 'Joint', value: 'JOINT' },
+    { label: 'Nuclear', value: 'NUCLEAR' },
+    { label: 'Other', value: 'OTHER' },
+  ];
+
+  const [openFamilyType, setOpenFamilyType] = useState(false);
+
+  const fetcher2 = async () => {
+    const response = await UserOnBoard();
+    return response.data?.data;
+  }
+
+  const { data: formFetchedData, isLoading } = useSWR('getOnboardData', fetcher2);
+
+
+  useEffect(() => {
+    if (formFetchedData) {
+      setFatherOccupation(formFetchedData.familyDetails?.fatherOccupation);
+      setMotherOccupation(formFetchedData.familyDetails?.motherOccupation);
+      setSisiters(JSON.stringify(formFetchedData.familyDetails?.siblings[0]?.count));
+      setBrothers(JSON.stringify(formFetchedData.familyDetails?.siblings[1]?.count));
+      setFamilyType(formFetchedData.familyType);
+    }
+  }, [formFetchedData]);
+
+  if (isLoading) {
+    return <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <ActivityIndicator size="large" />
+    </View>
+  }
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true);
+      const payload = {
+        familyDetails: {
+          motherOccupation: motherOccupation,
+          fatherOccupation: fatherOccupation,
+          'siblings': [
+            {
+              'relation': 'BROTHER',
+              'count': brothers
+            },
+            {
+              'relation': 'SISTER',
+              'count': sitsters
+            }
+          ]
+        },
+        familyType: familyType
+      };
+      const response = await UserOnBoard(payload);
+
+      setLoading(false);
+    }
+    catch (error) {
+
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+
 
   return (
     <View style={styles.container}>
       <TextInput
-        label="Father's Name"
-        value={fatherName}
-        onChangeText={setFatherName}
+        label="Father's Occupation"
+        value={fatherOccupation}
+        onChangeText={setFatherOccupation}
         style={styles.input}
       />
       <TextInput
-        label="Mother's Name"
-        value={motherName}
-        onChangeText={setMotherName}
+        label="Mother's Occupation"
+        value={motherOccupation}
+        onChangeText={setMotherOccupation}
         style={styles.input}
       />
       <TextInput
-        label="Number of Siblings"
-        value={siblings}
+        label="Number of Sisters"
+        value={sitsters}
         keyboardType="numeric"
-        onChangeText={setSiblings}
+        onChangeText={setSisiters}
         style={styles.input}
       />
+
       <TextInput
-        label="Family Type (e.g., Nuclear/Joint)"
-        value={familyType}
-        onChangeText={setFamilyType}
+        label="Number of Brothers"
+        value={brothers}
+        keyboardType="numeric"
+        onChangeText={setBrothers}
         style={styles.input}
       />
-      <Button mode="contained" onPress={() => alert('Family Details Saved!')}>
+
+
+
+      <DropDownPicker
+        open={openFamilyType}
+        value={familyType}
+        items={familyTypeArray}
+        setOpen={setOpenFamilyType}
+        style={{
+          zIndex: 1,
+          backgroundColor: '#fff3f4',
+          marginBottom: 26
+        }}
+        setValue={(callback) => {
+          const value = typeof callback === 'function' ? callback(familyType) : callback;
+          setFamilyType(value);
+        }}
+        placeholder="Select Family Type"
+        direction=""
+      />
+      <Button 
+      loading={loading}
+      disabled={loading}
+      mode="contained" onPress={handleSubmit}>
         Save
       </Button>
     </View>
@@ -46,11 +147,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff3f4',
   },
   input: {
     marginBottom: 16,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff3f4',
   }
 });
 
