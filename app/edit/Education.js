e6ffec87e451975a3c1632407f6b765892c25658
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-29 21:33:10 +0530

    enhance Education component with data fetching, loading state, and improved styling

--- app/edit/Education.js
+++ app/edit/Education.js
@@ -1,12 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { TextInput, Button, ActivityIndicator } from 'react-native-paper';
+import useSWR from 'swr';
+import { UserOnBoard } from '../../services/endpoint';
 
 const Education = () => {
   const [highestQualification, setHighestQualification] = useState('');
   const [fieldOfStudy, setFieldOfStudy] = useState('');
   const [university, setUniversity] = useState('');
   const [graduationYear, setGraduationYear] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  const fetcher2 = async () => {
+    const response = await UserOnBoard();
+    return response.data?.data;
+  }
+
+  const { data: formFetchedData, isLoading } = useSWR('getOnboardData', fetcher2);
+
+  useEffect(() => {
+    if (formFetchedData) {
+      setHighestQualification(formFetchedData.education);
+      // setFieldOfStudy(formFetchedData.fieldOfStudy);
+      // setUniversity(formFetchedData.university);
+      // setGraduationYear(formFetchedData.graduationYear);
+    }
+  }
+    , [formFetchedData]);
+
+  const handleSubmit = async () => {
+    try {
+      setLoading(true);
+      const payload = {
+        education: highestQualification,
+        // fieldOfStudy,
+        // university,
+        // graduationYear,
+      };
+      const response = await UserOnBoard(payload);
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (isLoading) {
+    return <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <ActivityIndicator size="large" />
+    </View>
+  }
+
 
   return (
     <View style={styles.container}>
@@ -16,13 +65,13 @@ const Education = () => {
         onChangeText={setHighestQualification}
         style={styles.input}
       />
-      <TextInput
+      {/* <TextInput
         label="Field of Study"
         value={fieldOfStudy}
         onChangeText={setFieldOfStudy}
         style={styles.input}
-      />
-      <TextInput
+      /> */}
+      {/* <TextInput
         label="University/College"
         value={university}
         onChangeText={setUniversity}
@@ -34,8 +83,11 @@ const Education = () => {
         keyboardType="numeric"
         onChangeText={setGraduationYear}
         style={styles.input}
-      />
-      <Button mode="contained" onPress={() => alert('Education Details Saved!')}>
+      /> */}
+      <Button mode="contained"
+      loading={loading}
+      disabled={loading}
+      onPress={handleSubmit}>
         Save
       </Button>
     </View>
@@ -46,11 +98,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff3f4',
   },
   input: {
     marginBottom: 16,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff3f4',
   }
 });
 
