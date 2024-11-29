e3d33a4cc822d0c952be1b41cefb89e214861738
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-29 21:47:07 +0530

    add TopSection component for user profile display with avatar and name

---
+++ component/Profile/TopSection.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Avatar, Divider } from 'react-native-paper'
+import images from '../../constants/images'
+
+const TopSection = ({ data }) => {
+    return (
+        <View>
+        <View
+            style={{
+                padding: 10,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 20,
+            }}
+        >
+            <Avatar.Image size={70} source={
+                data?.avatar ? { uri: data.avatar } : images.NoUser
+            } />
+
+            <View>
+            <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+                {data?.firstName + " " + data?.lastName}
+            </Text>
+            <Text>{data?._id}</Text>
+            </View>
+        </View>
+        <Divider />
+        </View>
+    )
+}
+
+export default TopSection
+
+const styles = StyleSheet.create({})
