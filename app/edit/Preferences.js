be00ba065a70e4db68d2c0d78f72060725b3353e
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-29 21:33:20 +0530

    enhance Preferences component with data fetching, loading state, and improved form submission handling

--- app/edit/Preferences.js
+++ app/edit/Preferences.js
@@ -1,12 +1,67 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { TextInput, Button, ActivityIndicator } from 'react-native-paper';
+import { UserOnBoard } from '../../services/endpoint';
+import useSWR from 'swr';
 
 const Preferences = () => {
   const [preferredPartnerAge, setPreferredPartnerAge] = useState('');
   const [preferredLocation, setPreferredLocation] = useState('');
   const [preferredReligion, setPreferredReligion] = useState('');
   const [preferredHeight, setPreferredHeight] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  const fetcher2 = async () => {
+    const response = await UserOnBoard();
+    return response.data?.data;
+  }
+
+  const { data: formFetchedData, isLoading } = useSWR('getOnboardData', fetcher2);
+
+  useEffect(() => {
+    if (formFetchedData) {
+      setPreferredPartnerAge(formFetchedData?.partnerPreference?.preferredPartnerAge);
+      setPreferredLocation(formFetchedData?.partnerPreference?.preferredLocation);
+      setPreferredReligion(formFetchedData?.partnerPreference?.preferredReligion);
+      setPreferredHeight(formFetchedData?.partnerPreference?.preferredHeight);
+    }
+  }, [formFetchedData]);
+
+  const handleSubmit = async () => {
+    try{
+
+      setLoading(true);
+
+      const payload = {
+        partnerPreference: {
+          preferredPartnerAge,
+          preferredLocation,
+          preferredReligion,
+          preferredHeight,
+        },
+      };
+
+      const response = await UserOnBoard(payload);
+
+      setLoading(false);
+
+    }catch(error){
+
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  if (isLoading) {
+    return <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <ActivityIndicator size="large" />
+    </View>
+  }
 
   return (
     <View style={styles.container}>
@@ -34,7 +89,10 @@ const Preferences = () => {
         onChangeText={setPreferredHeight}
         style={styles.input}
       />
-      <Button mode="contained" onPress={() => alert('Preferences Saved!')}>
+      <Button mode="contained" 
+      loading={loading}
+      disabled={loading}
+      onPress={handleSubmit}>
         Save
       </Button>
     </View>
@@ -45,11 +103,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff3f4',
   },
   input: {
     marginBottom: 16,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff3f4',
   }
 });
 
