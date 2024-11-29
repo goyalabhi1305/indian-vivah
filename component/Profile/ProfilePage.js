cdccc287553babde362a8c6be853ab2e3cd5bcec
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-29 21:46:58 +0530

    enhance ProfilePage component with user profile fetching, loading state, and improved layout with TopSection integration

--- component/Profile/ProfilePage.js
+++ component/Profile/ProfilePage.js
@@ -2,16 +2,57 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import CarouselCards from './CrouselCards'
 import ProfileSection from './ProfileSection'
+import useSWR from 'swr'
+import { GetProfileDetailUser } from '../../services/endpoint'
+import { useLocalSearchParams } from 'expo-router'
+import TopSection from './TopSection'
+import { ActivityIndicator } from 'react-native-paper'
 
 const ProfilePage = () => {
+
+  const {id} = useLocalSearchParams()
+  console.log(id)
+
+  const fetcher = async () => {
+    const response = await GetProfileDetailUser(id)
+    return response.data?.data
+  }
+
+  const { data, isLoading, error } = useSWR(
+    id ? `fetchUserProfileDetails${id}` : null,
+    fetcher)
+
+  if (isLoading) {
+    return <View style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '500'
+    }}>
+      <ActivityIndicator size="large" />
+    </View>
+  }
+
+
   return (
-    <ScrollView>
-      <CarouselCards />
-      <ProfileSection />
+    <ScrollView
+    contentContainerStyle={styles.contentContainer}
+    >
+      <TopSection data={data} />
+      <CarouselCards 
+      data={data}
+      />
+      <ProfileSection 
+        data={data}
+      />
     </ScrollView>
   )
 }
 
 export default ProfilePage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  contentContainer: {
+    backgroundColor: '#FFF3F4'
+  },
+})
