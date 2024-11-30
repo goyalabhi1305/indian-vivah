18997b544f5d0223f73d65fb74ae6c1eb8f41fae
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-11-30 21:33:16 +0530

    update PremiumComponent background color for improved visibility

--- component/PremiumComponent.js
+++ component/PremiumComponent.js
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff3f4',
     justifyContent: 'center',
   },
   header: {
