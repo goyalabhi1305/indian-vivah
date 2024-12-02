9e0d711ca715a162d9a2e05db4386dacf881195d
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-12-02 13:12:31 +0530

    add image viewing functionality to CarouselCards component

--- component/Profile/CrouselCards.js
+++ component/Profile/CrouselCards.js
@@ -2,26 +2,63 @@ import React from 'react'
 import { View } from "react-native"
 import Carousel from 'react-native-snap-carousel'
 import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from './CarouselCardItem'
+import ImageView from "react-native-image-viewing";
 // import data from './data'
 
-const CarouselCards = ({data}) => {
+const CarouselCards = ({ data }) => {
     const isCarousel = React.useRef(null)
-console.log(data?.fivePics)
+    const [visible, setIsVisible] = React.useState(false);
+    const [selectedImageIndex, setSelectedImageIndex] = React.useState(0);
+    const handleImagePress = (index) => {
+        console.log("index", index)
+        setSelectedImageIndex(index % 
+        data?.fivePics?.length
+        );
+        setIsVisible(true);
+    };
+    const [images, setImages] = React.useState([]);
+
+    React.useEffect(() => {
+        setImages(data?.fivePics?.map((item) => ({ uri: item.pic }))
+            || []);
+    }
+        , [data]);
+
+console.log("images", images)
+
     return (
         <View
         >
             <Carousel
-               layout={'default'} layoutCardOffset={`9`} 
+                layout={'default'} 
                 ref={isCarousel}
                 data={data?.fivePics || []}
-                renderItem={CarouselCardItem}
+                renderItem={
+                    ({ item, index }) => (
+                        <CarouselCardItem
+                            item={item}
+                            index={index}
+                            handleImagePress={() => handleImagePress(index)}
+                        />
+                    )
+                }
                 sliderWidth={SLIDER_WIDTH}
                 itemWidth={ITEM_WIDTH}
-                inactiveSlideShift={0}
-                inactiveSlideScale={0.95}
+                // inactiveSlideShift={0}
+                // inactiveSlideScale={0.95}
                 // inactiveSlideShift={0}
                 useScrollView={true}
-                loop={true}
+                // loop={true}
+                firstItem={0} 
+            />
+
+            <ImageView
+                images={images}
+                imageIndex={
+                    selectedImageIndex
+                }
+                visible={visible}
+                onRequestClose={() => setIsVisible(false)}
             />
         </View>
     )
@@ -29,3 +66,45 @@ console.log(data?.fivePics)
 
 
 export default CarouselCards
+
+
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+// import { useState } from "react";
+//
+
+// const CrouselCards = () => {
+//     const [visible, setIsVisible] = useState(true);
+
+//     const images = [
+//         {
+//             uri: "https://images.unsplash.com/photo-1571501679680-de32f1e7aad4",
+//         },
+//         {
+//             uri: "https://images.unsplash.com/photo-1573273787173-0eb81a833b34",
+//         },
+//         {
+//             uri: "https://images.unsplash.com/photo-1569569970363-df7b6160d111",
+//         },
+//     ];
+
+
+//     return (
+//         <View>
+
+//             <ImageView
+//                 images={images}
+//                 imageIndex={0}
+//                 visible={visible}
+//                 onRequestClose={() => setIsVisible(false)}
+//             />
+//         </View>
+//     )
+// }
+
+// export default CrouselCards
+
+// const styles = StyleSheet.create({})
+
+
+
