54f78e1de13a663f69c031fe502d3af6c2709228
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2024-12-05 16:44:04 +0530

    feat(plan): add subscription plans with pricing and features

---
+++ utils/plan.js
@@ -0,0 +1,180 @@
+const plan = [
+        {
+            "name": "Free plan",
+            "planId": "SVFREE",
+            "discountedPercentage": 0,
+            "durations": [
+                {
+                    "durationName": "1 Month",
+                    "price": 1244,
+                    "priceUsd": null
+                },
+                {
+                    "durationName": "3 Months",
+                    "price": 3500,
+                    "priceUsd": null
+                },
+                {
+                    "durationName": "Lifetime",
+                    "price": 10500,
+                    "priceUsd": null
+                }
+            ],
+            "features": {
+                "spotlights": {
+                    "availableLimit": 3,
+                    "included": true,
+                    "featureName": "Spotlights"
+                },
+                "profileViewsPerDay": {
+                    "availableLimit": 20,
+                    "included": true,
+                    "featureName": "Profile Views per Day"
+                },
+                "superInterest": {
+                    "availableLimit": 20,
+                    "included": true,
+                    "featureName": "Super Interest"
+                },
+                "relationshipManager": {
+                    "availableLimit": null,
+                    "included": false,
+                    "featureName": "Relationship Manager"
+                }
+            }
+        },
+        {
+            "name": "Basic",
+            "planId": "plan_BASIC",
+            "discountedPercentage": 0,
+            "durations": [
+                {
+                    "durationName": "1 Month",
+                    "price": 1244,
+                    "priceUsd": null
+                },
+                {
+                    "durationName": "3 Months",
+                    "price": 3500,
+                    "priceUsd": null
+                },
+                {
+                    "durationName": "Lifetime",
+                    "price": 10500,
+                    "priceUsd": null
+                }
+            ],
+            "features": {
+                "spotlights": {
+                    "availableLimit": 3,
+                    "included": true,
+                    "featureName": "Spotlights"
+                },
+                "profileViewsPerDay": {
+                    "availableLimit": 20,
+                    "included": true,
+                    "featureName": "Profile Views per Day"
+                },
+                "superInterest": {
+                    "availableLimit": 20,
+                    "included": true,
+                    "featureName": "Super Interest"
+                },
+                "relationshipManager": {
+                    "availableLimit": null,
+                    "included": false,
+                    "featureName": "Relationship Manager"
+                }
+            }
+        },
+        {
+            "name": "Mid",
+            "planId": "plan_MID",
+            "discountedPercentage": 0,
+            "durations": [
+                {
+                    "durationName": "1 Month",
+                    "price": 1945,
+                    "priceUsd": null
+                },
+                {
+                    "durationName": "3 Months",
+                    "price": 4500,
+                    "priceUsd": null
+                },
+                {
+                    "durationName": "Lifetime",
+                    "price": 13900,
+                    "priceUsd": null
+                }
+            ],
+            "features": {
+                "spotlights": {
+                    "availableLimit": 6,
+                    "included": true,
+                    "featureName": "Spotlights"
+                },
+                "profileViewsPerDay": {
+                    "availableLimit": 30,
+                    "included": true,
+                    "featureName": "Profile Views per Day"
+                },
+                "superInterest": {
+                    "availableLimit": 40,
+                    "included": true,
+                    "featureName": "Super Interest"
+                },
+                "relationshipManager": {
+                    "availableLimit": null,
+                    "included": true,
+                    "featureName": "Relationship Manager"
+                }
+            }
+        },
+        {
+            "name": "Super",
+            "planId": "plan_SUPER",
+            "discountedPercentage": 0,
+            "durations": [
+                {
+                    "durationName": "1 Month",
+                    "price": 2555,
+                    "priceUsd": null
+                },
+                {
+                    "durationName": "3 Months",
+                    "price": 6555,
+                    "priceUsd": null
+                },
+                {
+                    "durationName": "Lifetime",
+                    "price": 20000,
+                    "priceUsd": null
+                }
+            ],
+            "features": {
+                "spotlights": {
+                    "availableLimit": 10,
+                    "included": true,
+                    "featureName": "Spotlights"
+                },
+                "profileViewsPerDay": {
+                    "availableLimit": 60,
+                    "included": true,
+                    "featureName": "Profile Views per Day"
+                },
+                "superInterest": {
+                    "availableLimit": 90,
+                    "included": true,
+                    "featureName": "Super Interest"
+                },
+                "relationshipManager": {
+                    "availableLimit": null,
+                    "included": true,
+                    "featureName": "Relationship Manager"
+                }
+            }
+        }
+    ]
+
+export default plan;
